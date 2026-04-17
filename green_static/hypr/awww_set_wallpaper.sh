d92816057e0b29aa3a231f058f894f9fada12ff2
nuus <peter@ptak.dev>
2026-04-17 15:17:14 +0200

    revamp awww cycle eanimation

--- green_static/hypr/awww_set_wallpaper.sh
+++ green_static/hypr/awww_set_wallpaper.sh
@@ -5,13 +5,18 @@ state_dir="${XDG_RUNTIME_DIR:-/tmp}/green_static"
 state_file="$state_dir/current_wallpaper.path"
 fallback_wallpaper="$HOME/.config/hypr/wallpaper.jpg"
 wallpaper="${1:-}"
-transition_type="${AWWW_TRANSITION_TYPE:-random}"
-transition_duration="${AWWW_TRANSITION_DURATION:-0.8}"
-transition_fps="${AWWW_TRANSITION_FPS:-90}"
-transition_step="${AWWW_TRANSITION_STEP:-170}"
+if [ -n "${AWWW_TRANSITION_TYPE:-}" ]; then
+  transition_type="$AWWW_TRANSITION_TYPE"
+else
+  transition_types=(wipe wave wipe wave fade)
+  transition_type="${transition_types[$((RANDOM % ${#transition_types[@]}))]}"
+fi
+transition_duration="${AWWW_TRANSITION_DURATION:-0.55}"
+transition_fps="${AWWW_TRANSITION_FPS:-120}"
+transition_step="${AWWW_TRANSITION_STEP:-210}"
 transition_angle="${AWWW_TRANSITION_ANGLE:-$((RANDOM % 360))}"
-transition_wave="${AWWW_TRANSITION_WAVE:-$((12 + RANDOM % 18)),$((6 + RANDOM % 12))}"
-transition_bezier="${AWWW_TRANSITION_BEZIER:-0.85,0.00,0.15,1.00}"
+transition_wave="${AWWW_TRANSITION_WAVE:-$((8 + RANDOM % 10)),$((4 + RANDOM % 6))}"
+transition_bezier="${AWWW_TRANSITION_BEZIER:-0.92,0.00,0.10,1.00}"
 
 mkdir -p "$state_dir"
 
